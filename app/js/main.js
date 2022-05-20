7b6c7854c094192ff0cd699638c4cd61576667c4
AlexIstra1989 <alex888fcsm@gmail.com>
2022-05-20 23:44:09 +0300

    added about and gallery pages

--- app/js/main.js
+++ app/js/main.js
@@ -1,28 +1,33 @@
 $(function () {
-  $('.header__btn').on('click', function(){
-   $('.rightside-menu').removeClass('rightside-menu--close');
-  });
-  $('.rightside-menu__close').on('click', function(){
-   $('.rightside-menu').addClass('rightside-menu--close');
-  });
-  
+ $('.header__btn').on('click', function () {
+  $('.rightside-menu').addClass('rightside-menu--close');
+ });
+
+ $('.rightside-menu__close').on('click', function () {
+  $('.rightside-menu').removeClass('rightside-menu--close');
+ });
+
   $('.top__slider').slick({
    arrows: false,
    dots: true,
    fade: true,
   });
 
-  var mixer = mixitup('.gallery__inner', {
+  var mixer = mixitup('.gallery__inner',  {
    load: {
     filter: '.living'
    },
    animation: {
     effects: 'fade translateZ(-100px)',
     duration: 900,
-    effects: 'rotateY(-25deg)',
-    perspectiveDistance: '2000px'
+        effects: 'rotateY(-25deg)',
+        perspectiveDistance: '2000px'
    }
   });
 
+});
 
+$(function () {
+ var mixer = mixitup('.gallery-page__inner',  {
+ });
 });
